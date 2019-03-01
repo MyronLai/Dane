dad330bed3a7b8d1e616ce1bcb78d614315c46f3
ansonfoong <afoong@albany.edu>
2019-03-01 16:20:48 -0500

    Add command validater function and branches to handle each command

--- dane.py
+++ dane.py
@@ -9,4 +9,23 @@ with open('config.json') as f: # LOAD JSON FILE
 
 CLIENT_TOKEN = data['token'] # STORE CLIENT TOKEN from JSON.
 
+def isValidCommand(msg, command):
+    return msg.startswith("?" + command)
+
+@client.event
+async def on_ready():
+    print('Logged in as ' + client.user.name)
+
+@client.event
+async def on_message(message):
+
+    if message.author.bot:
+        return
+    if isValidCommand(message.content, "help"): # True if command is '?help'
+        print("Help Command") # Display Help Directory
+    elif isValidCommand(message.content, "bot"): # True if command is '?bot'
+        print("Bot")
+    
+
+
 client.run(CLIENT_TOKEN) # Run the bot
