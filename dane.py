d62a659e05bdba9d5f5ae154480c399ca8e61d16
ansonfoong <afoong@albany.edu>
2019-05-16 07:55:14 -0400

    Updated code to support new discordpy version. Changed client.send_message() to channel.send()

--- dane.py
+++ dane.py
@@ -46,7 +46,7 @@ async def on_message_delete(message): # Print out a summary of the message delet
     time = utc_to_local(message.timestamp)
     id = message.id
 
-    embed = discord.Embed()
+    embed = discord.Embed(color=16007746)
 
     embed.title = "Message Deleted <" + id +">"
 
@@ -56,51 +56,46 @@ async def on_message_delete(message): # Print out a summary of the message delet
     embed.add_field(name="Message", value=msgString)
     embed.set_author(name=msgAuthor, icon_url=msgAuthor.avatar_url)
 
-    embed.color = 16007746
-
     channels = message.server.channels
     mod_channel = discord.utils.get(channels, name='mod-logs')
-    await client.send_message(mod_channel, embed=embed)
+    await mod_channel.send(embed=embed)
 
 @client.event
 async def on_member_join(member):
     member_log_channel = discord.utils.get(member.server.channels, name='member-log')
     authorName = member.name + "#" + member.discriminator + " <" + member.id + ">"
-    embed = discord.Embed()
-    embed.color = 4303348
+    embed = discord.Embed(color=4303348)
     embed.set_author(name=authorName, icon_url=member.avatar_url)
     embed.set_footer(text="User joined")
-    await client.send_message(member_log_channel, embed=embed)
+    # Add user to Great Dane Role. 
+    await member_log_channel.send(embed=embed)
 
 @client.event
 async def on_member_remove(member):
     member_log_channel = discord.utils.get(member.server.channels, name='member-log')
     authorName = member.name + "#" + member.discriminator + " <" + member.id + ">"
-    embed = discord.Embed()
-    embed.color = 16007489
+    embed = discord.Embed(color=16007489)
     embed.set_author(name=authorName, icon_url=member.avatar_url)
     embed.set_footer(text="User left")
-    await client.send_message(member_log_channel, embed=embed)
+    await member_log_channel.send(embed=embed)
 
 @client.event
 async def on_member_ban(member):
     member_log_channel = discord.utils.get(member.server.channels, name='mod-logs')
     authorName = member.name + "#" + member.discriminator + " <" + member.id + ">"
-    embed = discord.Embed()
-    embed.color = 16029762
+    embed = discord.Embed(color=16029762)
     embed.set_author(name=authorName, icon_url=member.avatar_url)
     embed.set_footer(text="User Banned")
-    await client.send_message(member_log_channel, embed=embed)
+    await member_log_channel.send(embed=embed)
 
 @client.event
 async def on_member_unban(server, user):
     member_log_channel = discord.utils.get(server.channels, name='mod-logs')
     authorName = user.name + "#" + user.discriminator + " <" + user.id + ">"
-    embed = discord.Embed()
-    embed.color = 8314597
+    embed = discord.Embed(color=8314597)
     embed.set_author(name=authorName, icon_url=user.avatar_url)
     embed.set_footer(text="User Unbanned")
-    await client.send_message(member_log_channel, embed=embed)
+    await member_log_channel.send(embed=embed)
 
 def utc_to_local(dt):
     if time.localtime().tm_isdst:
