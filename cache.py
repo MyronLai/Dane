5a2fa12db072d3b94f084d6637ce4acf0555090d
ansonfoong <afoong@albany.edu>
2019-06-13 19:59:01 -0400

    The script  will run a thread that will clear the cache every amount of seconds. The script checks if the course data has been cached, if not it will request the data and cache it

---
+++ cache.py
@@ -0,0 +1,35 @@
+from time import sleep
+from threading import Thread
+import datetime
+import threading
+
+class Cache(threading.Thread):
+
+    def __init__(self):
+        self._courseCache = {}
+        self.callback = self.clearCache
+        self.event = threading.Event()
+        self.time = 60
+        super(Cache, self).__init__()
+    
+    def run(self):
+        while not self.event.wait(self.time):
+            self.callback()
+
+    def clearCache(self):
+        print("The current time is: " + str(datetime.datetime.now()))
+        print("Clearing Cache...")
+        self._courseCache.clear()
+
+    def add(self, key, cached_data):
+        if key in self._courseCache:
+            print("Cached item already exists.")
+        else:
+            self._courseCache[key] = cached_data
+    
+    def get(self, key):
+        return self._courseCache[key] or None
+
+    def get_cache(self):
+        return self._courseCache
+
