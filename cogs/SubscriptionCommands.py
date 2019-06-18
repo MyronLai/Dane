c742ef372d79dee241d3f2857c1c5aec588d0980
ansonfoong <afoong@albany.edu>
2019-06-18 18:36:10 -0400

    Removed required "type" argument.

--- cogs/SubscriptionCommands.py
+++ cogs/SubscriptionCommands.py
@@ -9,50 +9,51 @@ class SubscriptionCommands(commands.Cog):
     @commands.command()
     async def subbed(self, ctx): # Show all channels the user is subscribed to   
         channels = await get_subscribed_channels(ctx.author.id, self.client.database)
-        voice_channel_list = []
-        for channel in channels:
-            res = discord.utils.get(ctx.guild.voice_channels, id=int(channel[0]))
-            if res is not None:
-                voice_channel_list.append(res)
-        
-        description = ''
-        for channel in voice_channel_list:
-            description += "**Channel:** {} **ID:** {}\n".format(channel.name, channel.id)
-        
         embed=discord.Embed()
         embed.title='Subscribed Channels'
         embed.set_author(name="{}#{}".format(ctx.author.name, ctx.author.discriminator), icon_url=ctx.author.avatar_url)
-        embed.description=description
-        embed.color=1733275
-        await ctx.channel.send(embed=embed)
+        if channels is not None:
+            voice_channel_list = []
+            for channel in channels:
+                res = discord.utils.get(ctx.guild.voice_channels, id=int(channel[0]))
+                if res is not None:
+                    voice_channel_list.append(res)
+            description = ''
+            for channel in voice_channel_list:
+                description += "**Channel:** {} **ID:** {}\n".format(channel.name, channel.id)
+            
+            embed.description=description
+            embed.color=1733275
+            await ctx.channel.send(embed=embed)
+        else:
+            embed.description='You are not subscribed to any channels.'
+            embed.color=1733275
+            await ctx.channel.send(embed=embed)
     @commands.command()
-    async def subscribe(self, ctx, channel_type, *args):
+    async def subscribe(self, ctx, *args):
         voice_channels=ctx.guild.voice_channels
         embed=discord.Embed()
         if len(args) == 0:
-            if channel_type.lower() == 'voice':
-                embed.title='List of all Voice Channels and their IDs'
-                embed.color=5904098
-                description=''
-                embed.set_footer(text='Enter the ID of the voice channel you wish to subscribe to.', icon_url='https://cdn2.iconfinder.com/data/icons/metro-uinvert-dock/256/Microphone_1.png')
-                for channel in voice_channels:
-                    description += "**Name:** " + channel.name +  " - **ID:** " + "__" + str(channel.id) + "__\n"
-                embed.description=description
-                embed.set_author(name=self.client.user.name, icon_url=self.client.user.avatar_url)
-                await ctx.channel.send(embed=embed)
-                
-                def check(msg):
-                    try:
-                        return msg.author.id == ctx.author.id and discord.utils.get(voice_channels, id=int(msg.content)) is not None
-                    except Exception as error:
-                        print(error)
+            embed.title='List of all Voice Channels and their IDs'
+            embed.color=5904098
+            description=''
+            embed.set_footer(text='Enter the ID of the voice channel you wish to subscribe to.', icon_url='https://cdn2.iconfinder.com/data/icons/metro-uinvert-dock/256/Microphone_1.png')
+            for channel in voice_channels:
+                description += "**Name:** " + channel.name +  " - **ID:** " + "__" + str(channel.id) + "__\n"
+            embed.description=description
+            embed.set_author(name=self.client.user.name, icon_url=self.client.user.avatar_url)
+            await ctx.channel.send(embed=embed)
+            
+            def check(msg):
+                try:
+                    return msg.author.id == ctx.author.id and discord.utils.get(voice_channels, id=int(msg.content)) is not None
+                except Exception as error:
+                    print(error)
 
-                response = await self.client.wait_for('message', check=check)
-                # Now subscribe the user to the database.
-                await subscribe_user(response.content, ctx, self.database)
+            response = await self.client.wait_for('message', check=check)
+            # Now subscribe the user to the database.
+            await subscribe_user(response.content, ctx, self.database)
                 
-            elif channel_type.lower() == 'text':
-                pass
         else: # This case is for Users that call the command with specified voice channel ids
             def filter_ids(iter): # Filter function to filter out all invalid Voice Channel Ids
                 if discord.utils.find(lambda channel:channel.id==int(iter), voice_channels) is not None:
@@ -77,6 +78,7 @@ class SubscriptionCommands(commands.Cog):
                 embed.set_footer(text='To unsubscribe, use ?unsubscribe <channel_id>')
                 embed.color=2342399
                 await ctx.channel.send(embed=embed)
+                
             except Exception as error:
                 print(error)
 
