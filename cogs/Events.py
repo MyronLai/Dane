1d3389be8060eb7c57ed748fa7907b63fc51ca04
ansonfoong <afoong@albany.edu>
2019-06-19 06:54:33 -0400

    Send messages to users ONLY if the person who joined the VC is in the whitelist.

--- cogs/Events.py
+++ cogs/Events.py
@@ -29,25 +29,34 @@ class DaneBotEvents(commands.Cog):
     
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-
+        
         if before.channel is None and after.channel is not None: # User joins a Voice Channel while not being in one.
             cursor = self.database.cursor()
+            # This query will give us ALL of the Clients that are subscribed to the voice channel some user just joined.
             cursor.execute("SELECT client_id FROM VoiceChannelSubscriptions WHERE channel_id={} AND isSubscribed={}".format(str(after.channel.id), 1))
             result = cursor.fetchall()
-            embed=discord.Embed()
-            for id in result: # Loop through each id in the result set.
+            for id in result: # Loop through each id in the result set. Each ID represents the client subscribed.
                 current_member = discord.utils.find(lambda m: m.id==int(id[0]), member.guild.members) # Check if the  Member exists in Guild.
                 if current_member is not None and current_member.id != member.id: # If member is not none, and also not the  person who joined.
-                    embed.set_author(name=member.name,icon_url=member.avatar_url)
-                    embed.description="{} joined {}".format(member.name, after.channel.name)
-                    embed.set_footer(text=str(datetime.datetime.now().strftime("%A - %B %d at %I:%M:%S %p")))
-                    await current_member.send(embed=embed)
+                    # We need to get the CLIENT's whitelist.
+                    cursor.execute("SELECT whitelisted_user FROM VoiceChannelWhitelist WHERE channel_id={} AND client_id={} AND isWhitelisted={} AND isWhitelisted=1".format(str(after.channel.id), str(current_member.id), 1)) # Select whitelisted_user column, if the MEMBER who joined is in the whitelist, then we send a message.
+                    print("Whitelist for " + str(current_member) + " " + str(current_member.id))
+                    result = cursor.fetchall()
+                    ids = map(lambda n : n[0], result)
+                    
+                    if member.id in ids:
+                        print("Send message." + member.name + " is in the whitelist.")
+                        embed=discord.Embed()
+                        embed.set_author(name=member.name,icon_url=member.avatar_url)
+                        embed.description="{} joined {}".format(member.name, after.channel.name)
+                        embed.set_footer(text=str(datetime.datetime.now().strftime("%A - %B %d at %I:%M:%S %p")))
+                        await current_member.send(embed=embed)
                     
         elif before.channel is not None and after.channel is not None:
             print("User Switched.")
         elif before.channel is not None and after.channel is None:
             print("user left")
-
+    
     @commands.Cog.listener()
     async def on_message_delete(self, message): # Print out a summary of the message deleted
 
@@ -74,7 +83,6 @@ class DaneBotEvents(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        
         # Add user to database
         try:
             cursor = self.database.cursor()
@@ -125,10 +133,6 @@ class DaneBotEvents(commands.Cog):
         if message.author.bot:
             return
         if bucket.update_rate_limit(message):
-            print(message.author.name + ' is sending messages too fast!')
-            #cursor = self.database.cursor()
-            #cursor.execute("SELECT mute_role FROM Guilds where guild_id = " + str(message.guild.id))
-            
             muted_role = discord.utils.find(lambda role: role.name == 'Muted by Dane', message.guild.roles)
             if muted_role is not None:
                 print("Role exists.")
@@ -156,9 +160,8 @@ class DaneBotEvents(commands.Cog):
             await message.author.remove_roles(muted_role, reason="Unmuting...")
             return
         
-        elif message.content.startswith(self.client.command_prefix):
+        elif message.content.startswith(self.client.command_prefix): # If user is issuing a command, don't give them XP.
             return
-
         else:
             xp = generate_xp()
             # Give User XP
@@ -190,6 +193,14 @@ class DaneBotEvents(commands.Cog):
                         if currentLevel != 4:
                             currentLevel = 4
                             flag = True
+                    elif updatedXP > 3950 and currentXP < 8250:
+                        if currentLevel != 5:
+                            currentLevel = 5
+                            flag=True
+                    elif updatedXP > 8250 and currentXP < 13525:
+                        if currentLevel != 6:
+                            currentLevel = 6
+                            flag=True
                     else:
                         pass
                     # Update User XP and Level
@@ -223,7 +234,7 @@ def utc_to_local(dt):
         return dt - datetime.timedelta(seconds = time.timezone)
 
 def generate_xp():
-    return random.randint(1, 150)
+    return random.randint(1, 50)
 
 def setup(bot):
     bot.add_cog(DaneBotEvents(bot))
