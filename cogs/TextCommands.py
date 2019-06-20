411b5591e6cbedc812047d019cf321b39e113e50
ansonfoong <afoong@albany.edu>
2019-06-20 10:02:43 -0400

    Moved help command to  HelpMessageConfigurable  cog and registered help and sethelp to the same group.

--- cogs/TextCommands.py
+++ cogs/TextCommands.py
@@ -15,37 +15,7 @@ class TextCommands(commands.Cog):
     async def cmds(self, ctx):
         pass
 
-    '''
-        Notes:
-            If the admin issues this command, check if their record exists in the GuildHelpMsg table. If it doesn't, insert them with default help_msg {}
-    '''
-
-    @commands.command()
-    async def help(self, ctx):
-        embed=discord.Embed()
-        cursor = self.database.cursor()
-        cursor.execute("SELECT help_msg FROM GuildHelpMsg WHERE guild_id=" + str(ctx.guild.id))
-        result = cursor.fetchall()
-        empty = {
-            "msg" : "Set your help message!"
-        }
-        if len(result) == 0:
-            print("Guild does not exist. Add them with defaults.")
-            cursor.execute("INSERT INTO GuildHelpMsg (guild_id, help_msg) VALUES ({}, '{}')".format(str(ctx.guild.id), json.dumps(empty)))
-            embed.description='You do not have a help message set. ?sethelp'
-            await ctx.channel.send(embed=embed)
-        else:
-            if result[0][0] is None:
-                icon_url="https://cdn.discordapp.com/icons/{}/{}.png".format(ctx.guild.id, ctx.guild.icon)
-                embed.set_author(name=ctx.guild.name, icon_url=icon_url)
-                embed.description='No help message set!'
-                embed.set_footer(text='Please set a help message: ?sethelp')
-                await ctx.channel.send(embed=embed)
-            else:
-                icon_url="https://cdn.discordapp.com/icons/{}/{}.png".format(ctx.guild.id, ctx.guild.icon)
-                embed.set_author(name=ctx.guild.name, icon_url=icon_url)
-                embed.description=json.loads(result[0][0])['msg']
-                await ctx.channel.send(embed=embed)
+    
     '''
         Get a List of "Assignable" roles, i.e: Non-Admin, Non-Mod, Non-Permission roles.
         Iterate through the Guild Roles, check if it has Permissions.
